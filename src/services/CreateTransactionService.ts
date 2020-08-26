053adeb8ccfb426ba73901f9fdc54616849ffd52
Paulo Dias <paulodiasprivado@gmail.com>
2020-08-26 01:19:55 -0300

    Adicionando arquivos no repositorio!

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,37 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: RequestDTO ): Transaction {
+
+    if (!['income', 'outcome'].includes(type)){
+      throw new Error('Unavailable type.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value){
+      throw new Error('Unavailable balance.');
+    }
+
+    const transaction =  this.transactionsRepository.create({
+      title, value, type
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
